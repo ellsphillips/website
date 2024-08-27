e409c6860d71e94a9469cd09963b6d8970c4ff7c
ellsphillips <elliott.phillips.dev@gmail.com>
2024-08-27 18:54:18 +0100

    Dev: create global data object

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Hero from "@/components/hero";
-import { siteMetadata } from "@/lib/data";
 import BlurFadeText from "@/components/animation/blur-fade-text";
+import BlurFade from "@/components/animation/blur-fade";
+import { IconMapPin, IconBuildingBank } from "@tabler/icons-react";
+import DATA from "@/lib/data";
 
-const BLUR_FADE_DELAY = 0.04;
+const BLUR_FADE_DELAY = 0.1;
 
 export default function Home() {
   return (
@@ -14,16 +16,28 @@ export default function Home() {
             delay={BLUR_FADE_DELAY}
             className="text-3xl font-extrabold tracking-tight text-white md:text-[40px] md:leading-[1.1] lg:col-span-2 lg:text-[64px] lg:leading-[1.125em]"
             yOffset={8}
-            text={siteMetadata.title}
+            text={DATA.site.title}
           />
         </h1>
-        <div className="max-w-lg">
+        <p className="max-w-lg">
           <BlurFadeText
-            delay={BLUR_FADE_DELAY * 12}
+            delay={BLUR_FADE_DELAY * 5}
             className="mt-8 max-w-lg space-y-6 text-lg leading-[1.4] text-gray-300 md:max-w-xl lg:text-xl"
             yOffset={8}
-            text={siteMetadata.description}
+            text={DATA.site.description}
           />
+        </p>
+        <div className="pt-12 space-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 6}>
+            <span className="flex gap-2">
+              <IconMapPin /> Cardiff, Wales
+            </span>
+          </BlurFade>
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <span className="flex gap-2">
+              <IconBuildingBank /> Principal Software Engineer
+            </span>
+          </BlurFade>
         </div>
       </Hero>
 
--- lib/data.ts
+++ lib/data.ts
@@ -1,5 +1,27 @@
-export const siteMetadata = {
-  title: "Elliott Phillips",
-  description:
-    "Software Engineer who values learning and growing with people, teams, and technologies.",
+const DATA = {
+  site: {
+    title: "Elliott Phillips",
+    description:
+      "Software Engineer who values learning and growing with people, teams, and technologies.",
+  },
+  socials: [
+    {
+      name: "GitHub",
+      url: "www.github.com/ellsphillips",
+    },
+    {
+      name: "LinkedIn",
+      url: "www.linkedin.com/in/elliott-phillips",
+    },
+  ],
+  roles: [
+    {
+      title: "Senior Software Engineer",
+      company: "Lloyds Banking Group",
+      blurb:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi imperdiet non enim euismod auctor. Fusce ornare quam eget ex consectetur, in dapibus ligula pulvinar. Vivamus aliquet lacus sed euismod vulputate. Nunc pretium sit amet tortor et suscipit. Nullam nulla quam, tempus sit amet facilisis in, blandit et libero. Mauris congue, ante nec semper aliquam, risus lectus accumsan metus, vel consectetur augue orci id lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Praesent ultrices, orci ut blandit dapibus, est arcu sodales ex, nec porttitor nisi odio non lorem. Curabitur a dapibus nisl. Praesent varius, arcu pulvinar bibendum ornare, eros neque aliquam ante, non efficitur velit ligula eu est. In eu justo tincidunt, consectetur diam et, bibendum quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed dignissim id tortor quis imperdiet. Phasellus nec volutpat ligula, vel efficitur ligula.",
+    },
+  ],
 };
+
+export default DATA;
