33f4eb55659a116c7d62eabe5af1ad4229665d84
ellsphillips <elliott.phillips.dev@gmail.com>
2024-09-21 21:28:34 +0100

    Enhance: blog breadcrumb styling

--- components/blog/breadcrumbs.tsx
+++ components/blog/breadcrumbs.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
 import getAllBlogFrontmatter from "@/lib/content";
 
 export async function BlogBreadcrumbs({ page }: { page: string }) {
@@ -26,17 +27,28 @@ export async function BlogBreadcrumbs({ page }: { page: string }) {
           <BreadcrumbLink href="/">Home</BreadcrumbLink>
         </BreadcrumbItem>
         <BreadcrumbSeparator>
-          <SlashIcon />
+          <SlashIcon className="text-white/35 scale-x-50" strokeWidth={3} />
         </BreadcrumbSeparator>
         <BreadcrumbItem>
           <DropdownMenu>
-            <BreadcrumbLink href="/blog">Blog</BreadcrumbLink>
-            <DropdownMenuTrigger className="flex items-center gap-1">
-              <span className="scale-75 -ml-1">
+            <BreadcrumbLink href="/blog" className="-mr-1">
+              Blog
+            </BreadcrumbLink>
+            <DropdownMenuTrigger className="flex items-center justify-center hover:text-white">
+              <span className="grid place-content-center scale-75">
                 <ChevronDownIcon />
               </span>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
+            <DropdownMenuContent
+              align="start"
+              className="space-y-1 bg-slate-950"
+            >
+              <BreadcrumbLink href={"/blog"}>
+                <DropdownMenuItem>&lt;- All blogs</DropdownMenuItem>
+              </BreadcrumbLink>
+
+              <Separator />
+
               {posts.map((post) => (
                 <BreadcrumbLink href={`/blog/${post.slug}`}>
                   <DropdownMenuItem key={post.slug}>
@@ -48,7 +60,7 @@ export async function BlogBreadcrumbs({ page }: { page: string }) {
           </DropdownMenu>
         </BreadcrumbItem>
         <BreadcrumbSeparator>
-          <SlashIcon />
+          <SlashIcon className="text-white/35 scale-x-50" strokeWidth={3} />
         </BreadcrumbSeparator>
         <BreadcrumbItem>
           <BreadcrumbPage>{page}</BreadcrumbPage>
